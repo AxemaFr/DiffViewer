b381d6fc3770cbf22ad541489fb0cc392aca22ab
AxemaFr <axema@inbox.ru>
2020-10-23 15:44:12 +0300

    task4: removed fixtures to correct folder

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,7 +2,7 @@ import { test, expect } from '@jest/globals';
 import genDiff from '../src/index.js';
 
 test('flat diff', () => {
-  expect(JSON.stringify(genDiff('./tree1.json', './tree2.json'))).toBe(
+  expect(JSON.stringify(genDiff('./__fixtures__/tree1.json', './__fixtures__/tree2.json'))).toBe(
     JSON.stringify(['  - age: 14',
       '  + age: 12',
       '  + dima: blin',
